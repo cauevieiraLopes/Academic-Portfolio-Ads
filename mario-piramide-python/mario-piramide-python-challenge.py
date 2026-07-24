046a595a3eec01a1a7375fb6c2bbac5fe06481de
Cauê Lopes <caue.vieira.lopes@gmail.com>
2026-07-24 10:04:10 -0300

    Update print statement to repeat 'bloco' variable
    Modified the print statement to repeat the 'bloco' variable twice.

---
+++ mario-piramide-python/mario-piramide-python-challenge.py
@@ -0,0 +1,24 @@
+def solicitar_tamanho():
+    try:
+        tamanho = int(input("Tamanho da piramide: "))
+
+        if tamanho < 2:
+            raise Exception("Tamanho mínimo é 2")
+        elif tamanho > 8:
+            raise Exception("Tamanho máximo é 8")
+        else:
+            return tamanho
+        
+    except ValueError:
+            print("Digite um número inteiro!")
+
+def imprimir_tamanho(tamanho):
+    for i in range(1, tamanho + 1):
+        espaco = " " * (tamanho - i)
+        bloco = "#" * i
+      
+#acrestentou somente a variáveç bloco para se repetir duas vezes
+        print(f"{espaco}{bloco} {bloco}")
+
+altura = solicitar_tamanho()
+imprimir_tamanho(altura)
